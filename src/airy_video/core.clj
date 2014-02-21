bb51e9ed6496654bb354bfb93f3eae8e4cbf8af7
Viktor Lazarev <taurus101v@gmail.com>
2014-02-22 02:26:59 +0400

    Playing with widgets

--- src/airy_video/core.clj
+++ src/airy_video/core.clj
@@ -1,6 +1,30 @@
-(ns airy-video.core)
+(ns airy-video.core
+  (:use [seesaw.core]
+        [seesaw.chooser :only [choose-file]])
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def prog-name "Airy Video")
+
+(def prog-version "0.0.1")
+
+(def select-file-btn
+  (button :text "Select file"))
+
+(listen select-file-btn :action (fn [e]
+                                  (choose-file)
+                                  (config! select-file-btn :enabled? false)))
+
+(def formats-lbl (label "Select output format"))
+(def formats-cmb (combobox :model ["AVI", "MPG", "Matroska"]))
+
+(defn -main
+  [& args]
+  (invoke-later
+    (-> (frame
+          :title (str prog-name " " prog-version)
+          :on-close :exit
+          :content (vertical-panel :items [select-file-btn
+                                           formats-lbl
+                                           formats-cmb]))
+        pack!
+        show!)))
