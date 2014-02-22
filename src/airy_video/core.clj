a5deda577c5b55cf5c28c1d985cde5e172db42a7
Viktor Lazarev <taurus101v@gmail.com>
2014-02-22 14:30:09 +0400

    Playing with widgets

--- src/airy_video/core.clj
+++ src/airy_video/core.clj
@@ -8,23 +8,40 @@
 (def prog-version "0.0.1")
 
 (def select-file-btn
-  (button :text "Select file"))
+  (button
+    :text "Select file"
+    :minimum-size [150 :by 20]
+    :maximum-size [60 :by 20]
+    :valign :center
+    :halign :center))
 
-(listen select-file-btn :action (fn [e]
-                                  (choose-file)
-                                  (config! select-file-btn :enabled? false)))
+(listen select-file-btn
+        :action (fn [e]
+                  (choose-file)
+                  (config! select-file-btn :enabled? false)))
 
 (def formats-lbl (label "Select output format"))
 (def formats-cmb (combobox :model ["AVI", "MPG", "Matroska"]))
 
+(def main-panel
+  (grid-panel
+    :id main-panel
+    :items [select-file-btn
+            [:fill-v 250]
+            formats-lbl
+            formats-cmb]))
+
+(def main-window
+  (frame
+    :id :main-window
+    :title (str prog-name " " prog-version)
+    :on-close :exit
+    :size [610 :by 330]
+    :resizable? false
+    :content main-panel))
+
 (defn -main
   [& args]
   (invoke-later
-    (-> (frame
-          :title (str prog-name " " prog-version)
-          :on-close :exit
-          :content (vertical-panel :items [select-file-btn
-                                           formats-lbl
-                                           formats-cmb]))
-        pack!
+    (-> main-window
         show!)))
